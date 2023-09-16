22a444b1f33b0f6e0d946ddbe745a7b2022e25cf
BitBoy_ <abrahamavever@gmail.com>
2023-09-16 16:31:46 -0300

    Cambios realizados en el navbar y refactorización

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-window.addEventListener('load', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
-  
-    const savedTheme = localStorage.getItem('theme');
-    const defaultTheme = savedTheme === 'dark-theme' ? savedTheme : 'light-theme';
-  
-    body.classList.add(defaultTheme);
-    body.classList.remove(defaultTheme === 'light-theme' ? 'dark-theme' : 'light-theme');
-  
-    themeToggle.textContent = defaultTheme === 'light-theme' ? 'Tema oscuro' : 'Tema claro';
-  
-    themeToggle.addEventListener('click', () => {
-      body.classList.toggle('light-theme');
-      body.classList.toggle('dark-theme');
-  
-      const newTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
-      themeToggle.textContent = newTheme === 'light-theme' ? 'Tema oscuro' : 'Tema claro';
-      localStorage.setItem('theme', newTheme);
-    });
-  });
-  
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  const body = document.body;
+  const savedTheme = localStorage.getItem('theme');
+  const defaultTheme = savedTheme === 'dark-theme' ? savedTheme : 'light-theme';
+
+  function setTheme(theme) {
+    body.classList.remove('light-theme', 'dark-theme');
+    body.classList.add(theme);
+    themeToggle.textContent = theme === 'light-theme' ? 'Tema oscuro' : 'Tema claro';
+    localStorage.setItem('theme', theme);
+    const navbar = document.querySelector('.navbar');
+    navbar.setAttribute('data-bs-theme', theme === 'light-theme' ? 'light' : 'dark');
+    localStorage.setItem('navbarTheme', theme);
+  }
+
+  function toggleTheme() {
+    const currentTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+    const newTheme = currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+    setTheme(newTheme);
+  }
+
+  themeToggle.addEventListener('click', toggleTheme);
+
+  setTheme(defaultTheme);
+});
