a8004636a8cbe455c79b77df93a810f27a8e066f
BitBoy_ <abrahamavever@gmail.com>
2023-09-17 10:23:38 -0300

    cambiando el body de la clase dark-theme a "data-bs-theme"

--- script.js
+++ script.js
@@ -2,21 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
   const body = document.body;
   const savedTheme = localStorage.getItem('theme');
-  const defaultTheme = savedTheme === 'dark-theme' ? savedTheme : 'light-theme';
+  const defaultTheme = savedTheme === 'dark' ? savedTheme : 'light';
 
   function setTheme(theme) {
-    body.classList.remove('light-theme', 'dark-theme');
-    body.classList.add(theme);
-    themeToggle.textContent = theme === 'light-theme' ? 'Tema oscuro' : 'Tema claro';
+    body.setAttribute('data-bs-theme', theme);
+    themeToggle.textContent = theme === 'light' ? 'Tema oscuro' : 'Tema claro';
     localStorage.setItem('theme', theme);
     const navbar = document.querySelector('.navbar');
-    navbar.setAttribute('data-bs-theme', theme === 'light-theme' ? 'light' : 'dark');
+    navbar.setAttribute('data-bs-theme', theme);
     localStorage.setItem('navbarTheme', theme);
   }
 
   function toggleTheme() {
-    const currentTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
-    const newTheme = currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+    const currentTheme = body.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
   }
 
