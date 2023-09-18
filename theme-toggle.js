db0f7aac59b0002dbdaa32f50b7976d80a2ab1fb
BitBoy_ <abrahamavever@gmail.com>
2023-09-18 19:07:57 -0300

    Actualización del tema de la página con opción de cambio automático según la configuración del sistema

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,10 +1,27 @@
 function setTheme(theme) {
   const body = document.body;
-  body.setAttribute('data-bs-theme', theme);
-  localStorage.setItem('theme', theme);
-  const navbar = document.querySelector('.navbar');
-  navbar.setAttribute('data-bs-theme', theme);
-  localStorage.setItem('navbarTheme', theme);
+  const savedTheme = localStorage.getItem('theme');
+  const defaultTheme = savedTheme === 'dark' ? savedTheme : 'light';
+
+  if (theme === 'auto') {
+    if (window.matchMedia) {
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+      prefersDarkMode.addEventListener('change', event => {
+        const newTheme = event.matches ? 'dark' : 'light';
+        body.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+        const navbar = document.querySelector('.navbar');
+        navbar.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('navbarTheme', newTheme);
+      });
+    }
+  } else {
+    body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    const navbar = document.querySelector('.navbar');
+    navbar.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('navbarTheme', theme);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
