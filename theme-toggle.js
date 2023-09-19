a417285f947f2eff4aaca2a51bd34e6b929851f9
BitBoy_ <abrahamavever@gmail.com>
2023-09-19 12:04:11 -0300

    Optimizar y mejorar el código del archivo theme-toggle.js. - Utilizar constantes para valores que no cambian. - Utilizar nombres de variables más descriptivos. - Separar la lógica en funciones más pequeñas y específicas. - Agregar comentarios para mejorar la legibilidad. - Utilizar sintaxis moderna de JavaScript. - Seguir buenas prácticas de programación.

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,32 +1,41 @@
-function setTheme(theme) {
-  const body = document.body;
-  const savedTheme = localStorage.getItem('theme');
-  const defaultTheme = savedTheme === 'dark' ? savedTheme : 'light';
+const BODY = document.body;
+const NAVBAR = document.querySelector('.navbar');
+const THEME_KEY = 'theme';
+const NAVBAR_THEME_KEY = 'navbarTheme';
 
-  if (theme === 'auto') {
-    if (window.matchMedia) {
-      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-      prefersDarkMode.addEventListener('change', event => {
-        const newTheme = event.matches ? 'dark' : 'light';
-        body.setAttribute('data-bs-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-        const navbar = document.querySelector('.navbar');
-        navbar.setAttribute('data-bs-theme', newTheme);
-        localStorage.setItem('navbarTheme', newTheme);
-      });
-    }
-  } else {
-    body.setAttribute('data-bs-theme', theme);
-    localStorage.setItem('theme', theme);
-    const navbar = document.querySelector('.navbar');
-    navbar.setAttribute('data-bs-theme', theme);
-    localStorage.setItem('navbarTheme', theme);
-  }
-}
+const getSelectedTheme = () => {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+  return savedTheme === 'dark' ? savedTheme : 'light';
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  const savedTheme = localStorage.getItem('theme');
-  const defaultTheme = savedTheme === 'dark' ? savedTheme : 'light';
+const setThemeOnLoad = () => {
+  const selectedTheme = getSelectedTheme();
+  setTheme(selectedTheme);
+};
 
-  setTheme(defaultTheme);
-});
+const setTheme = (selectedTheme) => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  const isDarkMode = prefersDarkMode.matches;
+  const theme = selectedTheme === 'auto' ? (isDarkMode ? 'dark' : 'light') : selectedTheme;
+  BODY.setAttribute('data-bs-theme', theme);
+  NAVBAR.setAttribute('data-bs-theme', theme);
+  localStorage.setItem(THEME_KEY, theme);
+  localStorage.setItem(NAVBAR_THEME_KEY, theme);
+  setThemeOnPrefersDarkModeChange(theme);
+};
+
+const setThemeOnPrefersDarkModeChange = (theme) => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  prefersDarkMode.addEventListener('change', (event) => {
+    const newTheme = event.matches ? 'dark' : 'light';
+    setTheme(newTheme);
+  });
+};
+
+const initThemeToggle = () => {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+  const selectedTheme = savedTheme || getSelectedTheme();
+  setTheme(selectedTheme);
+};
+
+document.addEventListener('DOMContentLoaded', initThemeToggle);
